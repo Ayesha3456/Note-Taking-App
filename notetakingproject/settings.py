fa0f1eeb65cefbc52a94ff6211dd503df42213b5
Ayesha M <amruthanisha@gmail.com>
2024-11-11 12:21:29 +0530

    cha

--- notetakingproject/settings.py
+++ notetakingproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ri7s#*3wuljl#6iue*porcr@ord18j-^fmd4*b074zm3rt3_yq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
